d2633d8942c56b965d4c65705a9f183f751b8f15
Ratnesh Ravi <ratneshravi112@gmail.com>
2022-04-21 14:11:52 +0800

    Add project files.

---
+++ Controllers/ManagementsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WeeklyFirstTask.DataAccess;
+using WeeklyFirstTask.Models;
+
+namespace WeeklyFirstTask.Controllers
+{
+    public class ManagementsController : Controller
+    {
+        private readonly DataContext _conn;
+
+        public ManagementsController(DataContext conn)
+        {
+
+            _conn = conn;
+
+
+        }
+        public IActionResult DoctorDetails()
+        {
+            var data =  _conn.GetDoctors.ToList();
+            return View(data);
+
+        }
+        [HttpPost]
+        public IActionResult DoctorDetails(Doctor doctors)
+        {
+           
+            return View();
+
+        }
+        public IActionResult AppointDetails(int id)
+        {
+            //var getData = _conn.GetDoctors.SingleOrDefault(x => x.DId == id);
+            var getData = (from D in _conn.GetDoctors where D.DId == id select new Doctor
+            {
+                DName = D.DName,
+                Contact =D.Contact,
+                Specialization = D.Specialization,
+            }).SingleOrDefault();
+            return View(getData);
+
+        }
+    }
+}
---
+++ Models/Appointment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeeklyFirstTask.Models
+{
+    public class Appointment
+    {
+        public DateTime GetDateTime { get; set; }
+    }
+}
